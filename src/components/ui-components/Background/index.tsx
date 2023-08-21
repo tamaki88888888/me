6c216b065b0498c355f7842fd68d6b9ed3bdabe6
t.kou <koukoukokoko@icloud.com>
2023-08-21 09:27:13 +0900

    📝 add top background text 🔥delete projects page

--- src/components/ui-components/Background/index.tsx
+++ src/components/ui-components/Background/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import LogoIcon from "../LogoIcon/index";
 
 interface Props {}
 /**
- * @memo 背景UIを表現するコンポーネント
- * @todo 実実装
+ * @description 背景UIを表現するコンポーネント
+ * @todo アイコンのhref src 持ってくる
+ * @todo レイアウト整える
  */
 const Background: React.FC<Props> = () => {
-  return <>Background</>;
+  return (
+    <div className={"flex"}>
+      <LogoIcon href="" src="" />
+      <div className={"flex"}>
+        <a href="/about">// about</a>
+        <a href="/experience">// experience</a>
+        <a href="/works">// works</a>
+        <a href="/notes">// notes</a>
+        <a href="/contact">// contact</a>
+      </div>
+    </div>
+  );
 };
 
 export default Background;
