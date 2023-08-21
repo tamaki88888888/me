deeeb41230d69313e9031c09fe5cb32c9530835d
t.kou <koukoukokoko@icloud.com>
2023-08-21 09:18:49 +0900

    📝 add top page text

---
+++ src/components/ui-components/Background/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface Props {}
+/**
+ * @memo 背景UIを表現するコンポーネント
+ * @todo 実実装
+ */
+const Background: React.FC<Props> = () => {
+  return <>Background</>;
+};
+
+export default Background;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,24 @@
+import Header from "@/components/domain-components/SnsIconGloup/Header/index";
+import SnsIconGloup from "@/components/domain-components/SnsIconGloup/index";
+import Background from "@/components/ui-components/Background/index";
+
+/**
+ * @description TOPページ
+ * @todo Backgroundの実装
+ * @todo 共通部分をまとめて Layout に移動
+ * @todo styleをいい感じに当てる
+ */
 export default function Home() {
-  return <div>top</div>;
+  return (
+    <div>
+      <Background />
+      <Header />
+      <SnsIconGloup />
+      <div>
+        <h1>I'M KO TAMAKI</h1>
+        <p>I'm frontend engineer and ~~~~~~~</p>
+        <a href="/about">see and more about me →</a>
+      </div>
+    </div>
+  );
 }
