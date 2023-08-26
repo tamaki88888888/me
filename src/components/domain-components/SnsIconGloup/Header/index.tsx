87bfba2d667221eacd7ced72d66f0d4d7f78eeca
t.kou <koukoukokoko@icloud.com>
2023-08-26 12:29:56 +0900

    💄 style making header items centering

--- src/components/domain-components/SnsIconGloup/Header/index.tsx
+++ src/components/domain-components/SnsIconGloup/Header/index.tsx
@@ -10,24 +10,22 @@ interface Props {}
  */
 const Header: React.FC<Props> = () => {
   return (
-    <div
-      className={clsx([
-        "flex",
-        "justify-between",
-        "items-center",
-        "h-[200px]",
-        "p-5",
-        "w-[720px]",
-      ])}
-    >
-      <LogoIcon href="" src="/svg/Logo.svg" />
+    <div className={clsx(["flex", "justify-center"])}>
+      <div
+        className={clsx(
+          ["flex", "justify-between", "items-center"],
+          ["h-[200px]", "p-5", "w-[720px]"]
+        )}
+      >
+        <LogoIcon href="" src="/svg/Logo.svg" />
 
-      <div className={"flex gap-x-10"}>
-        <a href="/about">// about</a>
-        <a href="/experience">// experience</a>
-        <a href="/works">// works</a>
-        <a href="/notes">// notes</a>
-        <a href="/contact">// contact</a>
+        <div className={"flex gap-x-10"}>
+          <a href="/about">// about</a>
+          <a href="/experience">// experience</a>
+          <a href="/works">// works</a>
+          <a href="/notes">// notes</a>
+          <a href="/contact">// contact</a>
+        </div>
       </div>
     </div>
   );
