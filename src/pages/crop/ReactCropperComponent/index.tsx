4da3f9e48f98b1801f746b5ece5e1743e773b533
t.kou <koukoukokoko@icloud.com>
2023-08-31 15:21:49 +0900

    ✨ add crop page componets

---
+++ src/pages/crop/ReactCropperComponent/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface Props {}
+/**
+ * @memo react cropper component
+ * @url react cropper component
+ */
+const ReactCropperComponent: React.FC<Props> = () => {
+  return <div></div>;
+};
+
+export default ReactCropperComponent;
---
+++ src/pages/crop/index.tsx
@@ -0,0 +1,15 @@
+import WorkList from "@/components/domain-components/WorkList";
+import PageContainer from "@/components/ui-components/PageContainer";
+import clsx from "clsx";
+import Layout from "../Layout/index";
+
+export default function Home() {
+  return (
+    <div>
+      <Layout>
+        {/* コンテナーの役割 */}
+        <PageContainer>{/* 画像をクロップするテスト */}</PageContainer>
+      </Layout>
+    </div>
+  );
+}
