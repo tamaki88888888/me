34c887f0d60181fa26815ecb31d87389b8f44805
t.kou <koukoukokoko@icloud.com>
2023-09-02 00:02:41 +0900

    ✨ add crop page componets

--- src/pages/crop/ReactCropperComponent/index.tsx
+++ src/pages/crop/ReactCropperComponent/index.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import Cropper from "react-easy-crop";
 
 interface Props {}
 /**
  * @memo react cropper component
  * @url react cropper component
  */
-const ReactCropperComponent: React.FC<Props> = () => {
-  return <div></div>;
+
+//クロッパーに画像や、useStateを渡すことで色々できるようになる
+
+const ReactCropperComponent: React.FC = () => {
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(0);
+  const onCropComplete = () => {
+    console.log("aa");
+  };
+  return (
+    <div>
+      <Cropper
+        image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
+        crop={crop}
+        zoom={zoom}
+        rotation={90}
+        aspect={4 / 3}
+        onCropChange={setCrop}
+        onCropComplete={onCropComplete}
+        onZoomChange={setZoom}
+      />
+    </div>
+  );
 };
 
 export default ReactCropperComponent;
--- src/pages/crop/index.tsx
+++ src/pages/crop/index.tsx
@@ -2,13 +2,16 @@ import WorkList from "@/components/domain-components/WorkList";
 import PageContainer from "@/components/ui-components/PageContainer";
 import clsx from "clsx";
 import Layout from "../Layout/index";
+import ReactCropperComponent from "./ReactCropperComponent";
 
 export default function Home() {
   return (
     <div>
       <Layout>
         {/* コンテナーの役割 */}
-        <PageContainer>{/* 画像をクロップするテスト */}</PageContainer>
+        <PageContainer>
+          <ReactCropperComponent />
+        </PageContainer>
       </Layout>
     </div>
   );
