f2fbf813f19aaa86f6b22739b80d47e261257af1
t.kou <koukoukokoko@icloud.com>
2023-09-02 16:43:48 +0900

    🚚 move components which mistakenly put under SnsIconGloup into correct

---
+++ src/components/domain-components/Footer/index.tsx
@@ -0,0 +1,33 @@
+import LogoIcon from "@/components/ui-components/LogoIcon/index";
+import React from "react";
+import clsx from "clsx";
+import SnsIconGloup from "../SnsIconGloup";
+
+interface Props {}
+/**
+ * @description ヘッダー
+ * @todo もし共有したい記事が増えてきたら、notesページへのリンク先を追加する
+ */
+const Footer: React.FC<Props> = () => {
+  return (
+    <>
+      <div
+        className={clsx(
+          ["flex", "justify-center"],
+          ["fixed", "w-full", "bottom-0"]
+        )}
+      >
+        <div
+          className={clsx(
+            ["flex", "justify-between", "items-center"],
+            ["h-[200px]", "w-[720px]"]
+          )}
+        >
+          <SnsIconGloup />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
