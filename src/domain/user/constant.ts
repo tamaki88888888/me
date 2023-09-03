faa91656f936af3ec5002f10db716ef35c7f4960
t.kou <koukoukokoko@icloud.com>
2023-09-03 15:57:47 +0900

    💬 text of work experience

--- src/domain/user/constant.ts
+++ src/domain/user/constant.ts
@@ -3,7 +3,7 @@ import { Project } from "./model";
 export const ME = {
   NAME: "ko tamaki",
   DETAIL: "hi, im ko tamaki",
-  THUMBNAIL: "/image/thumbnail2.png", // pathは含めない方がいのかな
+  THUMBNAIL: "/image/thumbnail2.png",
 } as const;
 
 export const SNS = {
@@ -13,20 +13,28 @@ export const SNS = {
   MY_BLOG: { URL: "https://developer-blog-xi.vercel.app/" },
 };
 
-//定数なんだけどマップした時にめちゃめちゃ違和感ある
 export const EXPERIENCES = {
   WANO: {
-    NAME: "Wano株式会社", //en ja した方がいいかな
+    NAME: "Wano株式会社",
+    POSITION: "Frontend Engineer",
     DURING: "2022.2 - present",
     WEB_SITE: "https://group.wano.co.jp/",
-    DETAIL: ``,
+    DETAIL: [
+      `中長規模のフロントエンド設計、開発を担当し、Figma React Next TypeScript などを用いて実装しました。`,
+      `自社サービスのフロントエンド保守を担当しました。`,
+    ],
     SKILL: "React Recoil Next TS Perl",
   },
   PLACOLE: {
     NAME: "株式会社プラコレ",
+    POSITION: "Frontend Engineer",
     DURING: "2020.4 - 2021.10",
     WEB_SITE: "https://pla-cole.co/",
-    DETAIL: ``,
+    DETAIL: [
+      `クライアントのウェブサイト開発と保守を担当し、主にHTML、CSS、Sass、JavaScript、jQueryを使用して本番コードを提供しました。さまざまなサイトに対するクロスブラウザの互換性とモバイル対応を確保するための品質保証テストを実施しました。`,
+      `自社メディアのSEO向上に向けた開発や運用保守を担当、Wordpressにおける amp の導入や、pageSpeed Insite などを用いた改善立案などを行いました。`,
+      `中規模(1y)サービスのフロントエンド設計〜開発を担当し、主にReact Redux (ReactN) を用いて実装しました。`,
+    ],
     SKILL: "React Redux WordPrss JQuery AMP",
   },
 };
@@ -34,6 +42,7 @@ export const EXPERIENCES = {
 export const SIDE_WORKS = {
   AKITSU: {
     NAME: "秋津道路株式会社コーポレートサイト",
+    POSITION: "Frontend Engineer",
     DURING: "2022",
     WEB_SITE: "https://akitsudoro.com/",
     DETAIL: `秋津道路株式会社のコーポーレートサイト`,
@@ -41,12 +50,8 @@ export const SIDE_WORKS = {
   },
 };
 
-//よくよく考えたら、そこまで紹介したい記事がなくて草
 export const NOTES = {};
 
-//オブジェクトのキーをキャピタルにするかキャメルケースにするか悩み中。
-//ただ、これはマップするためのものだから小文字でいいかも
-//コンストがあって、型にコンストを依存させるべきか、型が先なのかがわからない
 export const PROJECTS: { [K in string]: Project } = {
   AKITSU: {
     shouldShow: true,
--- src/pages/experience/index.tsx
+++ src/pages/experience/index.tsx
@@ -4,26 +4,19 @@ import PageContainer from "@/components/ui-components/PageContainer/index";
 import { clsx } from "clsx";
 import Layout from "../Layout/index";
 
-/**
- * @description EXPERIENCEページ
- */
 export default function Home() {
   return (
     <div>
       <Header />
       <Layout>
-        {/* コンテナーの役割 */}
         <PageContainer>
           <div
             className={clsx(
-              ["flex", "flex-col", "gap-y-4"], // レイアウト的にいらないはず
+              ["flex", "flex-col", "gap-y-4"],
               ["text-white", "z-index-10"]
             )}
           >
-            {/* 見出し */}
             <h2 className={clsx(["text-3xl"])}>Work Experience</h2>
-
-            {/* 詳細 */}
             <WorkExpeience />
           </div>
         </PageContainer>
